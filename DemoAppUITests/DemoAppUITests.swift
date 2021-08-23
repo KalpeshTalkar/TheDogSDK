e7e9410e093a9c83ba0b27e4f10b4b987b72a1a7
Kalpesh Talkar <kalpeshtalkar@gmail.com>
2021-08-23 22:56:55 +0530

    Added UI test

--- DemoAppUITests/DemoAppUITests.swift
+++ DemoAppUITests/DemoAppUITests.swift
@@ -7,36 +7,57 @@
 
 import XCTest
 
-class DemoAppUITests: XCTestCase {
+class BaseUITest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var app = XCUIApplication()
 
+    let defaultLaunchArguments: [String] = [
+        "-uiAutomationTest"
+    ]
+
+    override func setUp() {
+        super.setUp()
         // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+        // However, we want to run all the UI tests and later on see the test logs for failed tests.
+        continueAfterFailure = true
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app.launchArguments.append(contentsOf: [
+            "-uiAutomationTest"
+        ])
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    override func tearDown() {
+        app.terminate()
+        super.tearDown()
+    }
+
+    func takeScreenshot(name: String) {
+        let fullScreenshot = XCUIScreen.main.screenshot()
+        let screenshot = XCTAttachment(uniformTypeIdentifier: "public.png", name: "Screenshot-\(name)-\(UIDevice.current.name).png", payload: fullScreenshot.pngRepresentation, userInfo: nil)
+        screenshot.lifetime = .keepAlways
+        add(screenshot)
+    }
+}
+
+class DemoAppUITests: BaseUITest {
+
+    func testSomething() {
+        sleep(5)
+
+        let collectionViewsQuery = app.collectionViews
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 7).tap()
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 1).children(matching: .other).element.tap()
+
+        let breedsButton = app.navigationBars["Breed Images"].buttons["Breeds"]
+        breedsButton.tap()
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 10).children(matching: .other).element.tap()
+
+        let searchByBreedNameSearchField = app.searchFields["Search by breed name..."]
+        searchByBreedNameSearchField.tap()
+        app.staticTexts["Cancel"].tap()
+        searchByBreedNameSearchField.tap()
+        breedsButton.tap()
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 14).children(matching: .other).element.tap()
     }
 }
